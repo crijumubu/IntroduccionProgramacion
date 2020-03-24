79724c8d4dbe99aaf8592d4c56cf27207db43c6e
crijumubu <62572462+crijumubu@users.noreply.github.com>
2020-03-23 22:34:56 -0500

    Mi primer commit

--- main.py
+++ main.py
@@ -2,14 +2,10 @@
 """
 Created on Mon Mar 23 22:00:34 2020
 
-a=int(input("Ingrese el primer número"))
-c=int(input("Ingrese el segundo número"))
-print("El producto de los dos números que ingresó es:", a*b)
-print("El doble del primer número es:", 2*a)
-b=int(input("Ingrese un número"))
-print("El número que ingresó es", b)
-d=int(input("Ingrese un número para calcularle la raíz cuadrada"))
-print("La raíz cuadrada del número que ingresó es:", d**0.5)
+a=int(input("Ingrese un primer número"))
+c=int(input("Ingrese el tercer número"))
+print("El producto del primer y tercer número que ingresó es:", a*c)
+print("El doble del primer número que ingresó es:", 2*a)
 
 @author: user
 """
