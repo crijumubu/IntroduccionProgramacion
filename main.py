ae083b3c6aae83c17b8e8c7efca46550cae981e9
crijumubu <62572462+crijumubu@users.noreply.github.com>
2020-03-23 22:39:10 -0500

    Mi primera modificación

--- main.py
+++ main.py
@@ -3,9 +3,13 @@
 Created on Mon Mar 23 22:00:34 2020
 
 a=int(input("Ingrese un primer número"))
-c=int(input("Ingrese un segundo número"))
-print("El producto del primer y segundo número que ingresó es:", a*c)
+b=int(input("Ingrese un segundo número"))
+c=int(input("Ingrese un tercer número"))
+d=int(input("Ingrese un cuarto número"))
+print("El producto del primer y tercer número que ingresó es:", a*c)
 print("El doble del primer número que ingresó es:", 2*a)
+print("El segundo número que ingresó es", b)
+print("La raíz cuadrada del cuarto número que ingresó es:", d**0.5)
 
 @author: user
 """
