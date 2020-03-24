000a9d32e1f74d4447b8c7cb2ba241a35c284f05
crijumubu <62572462+crijumubu@users.noreply.github.com>
2020-03-23 22:15:53 -0500

    Mi primer commit

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 """
 Created on Mon Mar 23 22:00:34 2020
 
+a=int(input("Ingrese el primer número"))
+c=int(input("Ingrese el segundo número"))
+print("El producto de los dos números que ingresó es:", a*b)
+print("El doble del primer número es:", 2*a)
+
 @author: user
 """
 
