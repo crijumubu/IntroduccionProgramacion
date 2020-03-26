e8fe87ce6314cdc90605e47b730890b88718414f
crijumubu <62572462+crijumubu@users.noreply.github.com>
2020-03-26 11:43:12 -0500

    Solución simple terminada

--- main.py
+++ main.py
@@ -9,16 +9,15 @@ c=int(input("Ingrese el valor de c para la ecuación cuadrática: "))
 d=(b**2)-4*a*c
 
 if d>0:
-    x1=(-b+(d**1/2))/2*a
-    x2=(-b-(d*1/2))/2*a
+    x1=(-b+(d**0.5))/(2*a)
+    x2=(-b-(d**0.5))/(2*a)
     print("x1=",x1)
     print("x2=",x2)
-    if d==0:
-        print("d corresponde a:-b/2a")
-        if d<0:
-            print("No existe solución a la ecuación cuadrática dentro del dominio de los números reales")
-
-
+elif d==0:
+    d=-b/(2*a)
+    print("d corresponde a:-b/2a que es igual a:", d)
+else: 
+    print("No existe solución a la ecuación cuadrática dentro del dominio de los números reales")
 
 @author: user
 """
